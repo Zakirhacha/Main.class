efe65035741ef77f30c7e147d230bd9b333de634
zakirov.krt <zakirov.danil97@gmail.com>
2022-12-07 23:45:11 +0500

    добавили )))

---
+++ HomeWork/src/Main.java
@@ -0,0 +1,27 @@
+package HomeWork;
+// Написать программу, запрашивающую у пользователя число,
+// вывести в консоль максимальную цифру этого числа
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int maxNumber = 0;
+        System.out.print("Введите число: ");
+        int userNumber = scanner.nextInt();
+        if (userNumber < 0) {
+            userNumber *= -1;
+        }
+
+        for(; userNumber != 0; userNumber /= 10) {
+            int temp = userNumber % 10;
+            if (temp > maxNumber) {
+                maxNumber = temp;
+            }
+        }
+        System.out.print("\nМаксимальная цифра " + maxNumber);
+    }
+
+}
